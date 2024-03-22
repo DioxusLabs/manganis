b57161777e4141d887f73f3e3da3cd318fb73fd5
Evan Almloff <evanalmloff@gmail.com>
2024-03-22 16:41:49 -0500

    update the default serve directory to /

--- common/src/config.rs
+++ common/src/config.rs
@@ -3,7 +3,7 @@ use serde::{Deserialize, Serialize};
 use crate::cache::config_path;
 
 fn default_assets_serve_location() -> String {
-    "dist/".to_string()
+    "/".to_string()
 }
 
 /// The configuration for collecting assets
